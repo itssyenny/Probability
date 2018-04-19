1afe63aee447e7674e5b31d87535eb5b81eae03a
Syn <syennyw98@gmail.com>
2018-04-19 22:20:45 +0800

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,6 +1,8 @@
 import random
 import math
 
+#design random experiment to estimate the circumference ratio phi
+
 def randomize( nrand ):
 	n_inside = 0
 	min = -1
@@ -9,7 +11,7 @@ def randomize( nrand ):
 	for nloops in range(1, nrand):
 		xrand = random.randint(min, max)*2 - 1
 		yrand = random.randint(min, max)*2 - 1
-
+		
 		#find its distance from origin
 		rrand = math.sqrt(math.pow(2,xrand) + math.pow(2,yrand))
 		
@@ -18,10 +20,9 @@ def randomize( nrand ):
 			n_inside += 1
 		#end
 	#end
-	phi = (4 * n_inside)/nrand
-	print phi
-	return
+	return (n_inside/nrand)
 
 #Call the random function
 nrand = 4
-randomize(nrand)
+phi = 4 * randomize(nrand)
+print phi
