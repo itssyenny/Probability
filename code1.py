7ef46e381be7cdcf1c146009b0b8e1157c1cf1cb
Syn <syennyw98@gmail.com>
2018-04-19 22:04:03 +0800

    Adding Code 1

---
+++ code1.py
@@ -0,0 +1,26 @@
+import random
+
+def randomize( nrand ):
+	n_inside = 0
+	min = -1
+	max = 1
+
+	for nloops in range(1, nrand):
+		xrand = random.randint(min, max)*2 - 1
+		yrand = random.randint(min, max)*2 - 1
+
+		#find its distance from origin
+		rrand = xrand + yrand
+		
+		#it falls inside the circle
+		if rrand <= 1:
+			n_inside += 1
+		#end
+	#end
+	phi = (4 * n_inside)/nrand
+	print phi
+	return
+
+#Call the random function
+nrand = 4
+randomize(nrand)
